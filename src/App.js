99b65f88d456afd773f13295e6a2dd253f5876a9
Leonid <Lyunonin@yandex.ru>
2024-01-28 20:30:19 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
 import './App.css';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import wheelI from './колесо.png'
+import wheelII from './колесо2.png'
 
 function App() {
-  const [rot,setrot] = useState(0)
-  const style ={
-      position:"absolute",
-      transform: `translate(900px) rotate(${rot}deg)`,
-      width: '400px',
-      height: '400px',
+  const [rot, setrot] = useState(0)
+  const style = {
+    position: "absolute",
+    transform: `rotate(${rot}deg)`,
+    width: '400px',
+    height: '400px',
   }
-  useEffect(()=>{
-    const timer = setInterval(()=>
-    {
-      setrot(prev=>prev+1)
-    },1)
-    if(rot==360){
+  const style1 = {
+    position: "absolute",
+    transform: `rotate(${rot}deg)`,
+    width: '200px',
+    height: '200px',
+  }
+
+  
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setrot(prev => prev + 0.5)
+    }, 1)
+    if (rot == 360) {
       setrot(0)
     }
-    return ()=> clearInterval(timer)
+    return () => clearInterval(timer)
   })
   return (
     <div className="App">
-      <img src="колесо.png" className='bigwheel' style={style} alt=''/>
-      <img src='колесо2.png' style={style}alt=''/>
+      <div className='modalWindow'>
+        <div className='modalDiolog'>
+          <div className='modalHeader'>
+            <h3></h3>
+          </div>
+        </div>
+      </div>
+      <div className='header'>
+        <div className='Zagolovok'>
+          <p>Паровой двигатель</p>
+        </div>
+        <div className='avtor'><p>Nikolay Kutuzov</p></div>
+      </div>
+      <div className='main'>
+        <img src={wheelI} className='bigwheel' onClick={() => { console.log('cock'); }} style={style} alt='' />
+        <img src={wheelII} style={style1} alt='' />
+      </div>
     </div>
   );
 }
